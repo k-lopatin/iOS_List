ffa643b3f78654e8cf4d13a49de2c751918c1620
lk1195 <lk1195@ya.ru>
2014-10-14 14:24:26 +0400

    folders and core data

---
+++ List/Sourses/GUI/Controllers/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  List
+//
+//  Created by lk1195 on 10/9/14.
+//  Copyright (c) 2014 lk1195. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
